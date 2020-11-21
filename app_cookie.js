4a0687cdb5f6822b055e90ac75bbb350ae37fd9e
Gwang Hyun Ji <gwgw4578@gmail.com>
2020-11-21 10:02:14 +0900

    1121 nodejs cookie

---
+++ app_cookie.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+app.use(cookieParser());
+
+var products = {
+    1:{title:'The history of web 1'},
+    2:{title:'The next web 1'}
+};
+app.get('/products',function(req,res){
+    var output = '';
+    for(var name in products){
+        output += `
+        <li>
+        <a href="/cart/${name}">${products[name].title}
+        </li>
+        `
+    }
+    res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
+})
+
+app.get('/count', function(req,res){
+    if(req.cookies.count){
+    var count = parseInt(req.cookies.count);
+    } else {
+    var count = 0;
+    }
+    count = count+1;
+    res.cookie('count', count);
+    res.send('count : '+ count);
+});
+app.listen(3003, function(){
+    console.log('Connected 3003 port!!!');
+});
