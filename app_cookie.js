d5e0eb650b681a3cfdf2e72a48bd6c2d7250c222
Ji Gwang Hyun <gwgw4578@gmail.com>
2020-12-18 23:48:50 +0900

    1218 cookie signedCookies

--- app_cookie.js
+++ app_cookie.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
-app.use(cookieParser());
-
+app.use(cookieParser('2332423@#!521dsda'));
 var products = {
     1:{title:'The history of web 1'},
     2:{title:'The next web 1'}
@@ -20,8 +19,8 @@ app.get('/products',function(req,res){
 });
 app.get('/cart/:id', function(req, res){
     var id = req.params.id;
-    if(req.cookies.cart){
-        var cart = req.cookies.cart;
+    if(req.signedCookies.cart){
+        var cart = req.signedCookies.cart;
     } else {
         var cart = {};
     }
@@ -29,11 +28,11 @@ app.get('/cart/:id', function(req, res){
         cart[id] = 0;
     }
     cart[id] = parseInt(cart[id])+1;
-    res.cookie('cart', cart);
+    res.cookie('cart', cart , {signed:true});
     res.redirect('/cart');
 });
 app.get('/cart',function(req,res){
-    var cart = req.cookies.cart;
+    var cart = req.signedCookies.cart;
     if(!cart){
         res.send('Empty!');
     }else {
@@ -50,13 +49,13 @@ app.get('/cart',function(req,res){
 });
 
 app.get('/count', function(req,res){
-    if(req.cookies.count){
-    var count = parseInt(req.cookies.count);
+    if(req.signedCookies.count){
+    var count = parseInt(req.signedCookies.count);
     } else {
     var count = 0;
     }
     count = count+1;
-    res.cookie('count', count);
+    res.cookie('count', count , {signed:true});
     res.send('count : '+ count);
 });
 app.listen(3003, function(){
